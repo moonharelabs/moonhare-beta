d6b9b5c378b3a40ca1da9b88ef4d85cc510adbb1
Kavindu Santhusa <89186091+ksenginew@users.noreply.github.com>
2021-09-18 11:33:16 +0530

    build
    added rollup build
    see rollup.config.js

--- a.js
+++
@@ -1,4 +0,0 @@
-import Processor from './src';
-
-const p = new Processor();
-console.log(p._config);
---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import {terser} from 'rollup-plugin-terser';
+import typescript from '@rollup/plugin-typescript';
+import pkg from './package.json';
+
+export default {
+    input: 'src/index.ts',
+    plugins: [
+        terser(),
+        typescript(), // so Rollup can convert TypeScript to JavaScript
+    ],
+    output: [
+        { file: pkg.main, format: 'cjs', exports: 'named' },
+        { file: pkg.module, format: 'es' },
+        {
+            file: pkg.browser,
+            format: 'umd',
+            name: 'moonHare',
+            exports: 'named',
+        },
+    ],
+};
